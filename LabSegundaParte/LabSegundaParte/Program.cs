9dbc646e5d3cd7b030603437052fbb02c4692311
MarvinRivas07 <88003076+MarvinRivas07@users.noreply.github.com>
2021-08-30 16:16:54 -0600

    Programs

--- LabSegundaParte/LabSegundaParte/Program.cs
+++ LabSegundaParte/LabSegundaParte/Program.cs
@@ -10,6 +10,18 @@ namespace LabSegundaParte
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingresa tu nombre");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su sexo");
+            string sexo = Console.ReadLine();
+            Console.WriteLine("Ingrese su peso");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa tu altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+
         }
     }
 }
