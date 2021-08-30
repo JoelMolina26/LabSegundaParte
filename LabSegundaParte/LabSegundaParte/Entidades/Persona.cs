39585a46a6fafa7ff6f3e38f82cd0de82109f76d
MarvinRivas07 <88003076+MarvinRivas07@users.noreply.github.com>
2021-08-30 15:56:37 -0600

    Encapsulamiento

--- LabSegundaParte/LabSegundaParte/Entidades/Persona.cs
+++ LabSegundaParte/LabSegundaParte/Entidades/Persona.cs
@@ -8,5 +8,23 @@ namespace LabSegundaParte.Entidades
 {
     class Persona
     {
+        private string nombre;
+        private int edad;
+        private string sexo;
+        private double peso;
+        private double altura;
+
+        public Persona(string nombre, int edad, string sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
     }
 }
